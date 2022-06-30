2c7581e326bc32ab5f8ae1b863a88837e571a4dc
Kevin Pagenkämper <xoyz.productions@gmail.com>
2022-06-30 17:54:45 +0200

    Zest getting dynamic step by step

--- modules/Zest.py
+++ modules/Zest.py
@@ -6,11 +6,12 @@ class Zest:
         self.input      = x
         self.weights    = [
             np.random.rand(self.input.shape[1], len(y)),
-            np.random.rand(len(y), y.shape[1]),
-            np.random.rand(y.shape[1], y.shape[1]),
-            np.random.rand(y.shape[1], y.shape[1]),
-            np.random.rand(y.shape[1], len(y[0]))
+            np.random.rand(len(y), y.shape[1])
         ]
+        for _ in range(2):
+            self.weights.append(np.random.rand(y.shape[1], y.shape[1]))
+        self.weights.append(np.random.rand(y.shape[1], len(y[0])))
+        
         self.y          = y
         self.output     = np.zeros(self.y.shape)
     
@@ -35,12 +36,14 @@ class Zest:
         '''
         if x_in is None:
             x_in = self.input
+        
+        self.layers = []
 
-        self.layer1 = self.sigmoid(np.dot(x_in, self.weights[0]))
-        self.layer2 = self.sigmoid(np.dot(self.layer1, self.weights[1]))
-        self.layer3 = self.sigmoid(np.dot(self.layer2, self.weights[2]))
-        self.layer4 = self.sigmoid(np.dot(self.layer3, self.weights[3]))
-        self.output = self.sigmoid(np.dot(self.layer4, self.weights[4]))
+        self.layers.append(self.sigmoid(np.dot(x_in, self.weights[0])))
+        self.layers.append(self.sigmoid(np.dot(self.layers[0], self.weights[1])))
+        self.layers.append(self.sigmoid(np.dot(self.layers[1], self.weights[2])))
+        self.layers.append(self.sigmoid(np.dot(self.layers[2], self.weights[3])))
+        self.output = self.sigmoid(np.dot(self.layers[3], self.weights[4]))
     
     def backprop(self):
         '''
@@ -48,15 +51,15 @@ class Zest:
         '''
         d_weights = [0.0 for _ in range(len(self.weights))]
 
-        d_weights[4] = np.dot(self.layer4.T, (2 * (self.y - self.output) * self.sigmoid_derivative(self.output)))
+        d_weights[4] = np.dot(self.layers[3].T, (2 * (self.y - self.output) * self.sigmoid_derivative(self.output)))
 
-        d_weights[3] = np.dot(self.layer3.T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[4].T) * self.sigmoid_derivative(self.layer4)))
+        d_weights[3] = np.dot(self.layers[2].T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[4].T) * self.sigmoid_derivative(self.layers[3])))
 
-        d_weights[2] = np.dot(self.layer2.T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[3].T) * self.sigmoid_derivative(self.layer3)))
+        d_weights[2] = np.dot(self.layers[1].T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[3].T) * self.sigmoid_derivative(self.layers[2])))
 
-        d_weights[1] = np.dot(self.layer1.T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[2].T) * self.sigmoid_derivative(self.layer2)))
+        d_weights[1] = np.dot(self.layers[0].T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[2].T) * self.sigmoid_derivative(self.layers[1])))
 
-        d_weights[0] = np.dot(self.input.T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[1].T) * self.sigmoid_derivative(self.layer1)))
+        d_weights[0] = np.dot(self.input.T, (np.dot(2 * (self.y - self.output) * self.sigmoid_derivative(self.output), self.weights[1].T) * self.sigmoid_derivative(self.layers[0])))
 
         # Now update the weights
         for i in range(len(self.weights)):
